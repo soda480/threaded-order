bf3daf61fa3b7d064322d23d0dea23e5c6ebc75b
Emilio Reyes <emilio.reyes@intel.com>
2025-11-25 20:30:38 -0700

    feat: add colored formatter logger (#5)

--- examples/common.py
+++ examples/common.py
@@ -8,4 +8,4 @@ def runit(name):
     sleep = random.uniform(.5, 3.5)
     logger.debug(f'{name} running - sleeping {sleep:.2f}s')
     time.sleep(sleep)
-    logger.info(f'{name} completed')
+    logger.info(f'{name} PASSED')
