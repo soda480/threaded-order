8a01595e4d88bdcd0b9ea9b729f5bd37e1c13d44
Emilio Reyes <emilio.reyes@intel.com>
2025-12-02 18:42:01 -0700

    feat: filter across multiple tags (#13)

--- examples/test_dag.py
+++ examples/test_dag.py
@@ -34,34 +34,34 @@ def run(name, state, deps=None, fail=False):
 #   - test_01_06 has 2 children (layer2)
 #   - test_01_08 has 3 children (layer2)
 # ---------------------------------------------------------------------------
-@dmark(with_state=True, tag='layer1')
+@dmark(with_state=True, tags='layer1')
 def test_01_01(state): return run('test_01_01', state)
 
-@dmark(with_state=True, tag='layer1')
+@dmark(with_state=True, tags='layer1')
 def test_01_02(state): return run('test_01_02', state)
 
 # parent of 4 children in layer2
-@dmark(with_state=True, tag='layer1')
+@dmark(with_state=True, tags='layer1')
 def test_01_03(state): return run('test_01_03', state)
 
-@dmark(with_state=True, tag='layer1')
+@dmark(with_state=True, tags='layer1')
 def test_01_04(state): return run('test_01_04', state)
 
-@dmark(with_state=True, tag='layer1')
+@dmark(with_state=True, tags='layer1')
 def test_01_05(state): return run('test_01_05', state)
 
 # parent of 2 children in layer2
-@dmark(with_state=True, tag='layer1')
+@dmark(with_state=True, tags='layer1')
 def test_01_06(state): return run('test_01_06', state)
 
-@dmark(with_state=True, tag='layer1')
+@dmark(with_state=True, tags='layer1')
 def test_01_07(state): return run('test_01_07', state)
 
 # parent of 3 children in layer2
-@dmark(with_state=True, tag='layer1')
+@dmark(with_state=True, tags='layer1')
 def test_01_08(state): return run('test_01_08', state)
 
-@dmark(with_state=True, tag='layer1')
+@dmark(with_state=True, tags='layer1')
 def test_01_09(state): return run('test_01_09', state)
 
 # ---------------------------------------------------------------------------
@@ -73,34 +73,34 @@ def test_01_09(state): return run('test_01_09', state)
 #   - test_02_06 has 2 children (layer3)
 #   - test_02_08 has 2 children (layer3)
 # ---------------------------------------------------------------------------
-@dmark(with_state=True, after=['test_01_03'], tag='layer2')
+@dmark(with_state=True, after=['test_01_03'], tags='layer2')
 def test_02_01(state): return run('test_02_01', state, deps=['test_01_03'])
 
 # parent of 3 children in layer3
-@dmark(with_state=True, after=['test_01_03'], tag='layer2')
+@dmark(with_state=True, after=['test_01_03'], tags='layer2')
 def test_02_02(state): return run('test_02_02', state, deps=['test_01_03'])
 
-@dmark(with_state=True, after=['test_01_03'], tag='layer2')
+@dmark(with_state=True, after=['test_01_03'], tags='layer2')
 def test_02_03(state): return run('test_02_03', state, deps=['test_01_03'])
 
-@dmark(with_state=True, after=['test_01_03'], tag='layer2')
+@dmark(with_state=True, after=['test_01_03'], tags='layer2')
 def test_02_04(state): return run('test_02_04', state, deps=['test_01_03'])
 
-@dmark(with_state=True, after=['test_01_06'], tag='layer2')
+@dmark(with_state=True, after=['test_01_06'], tags='layer2')
 def test_02_05(state): return run('test_02_05', state, deps=['test_01_06'])
 
 # parent of 2 children in layer3
-@dmark(with_state=True, after=['test_01_06'], tag='layer2')
+@dmark(with_state=True, after=['test_01_06'], tags='layer2')
 def test_02_06(state): return run('test_02_06', state, deps=['test_01_06'])
 
-@dmark(with_state=True, after=['test_01_08'], tag='layer2')
+@dmark(with_state=True, after=['test_01_08'], tags='layer2')
 def test_02_07(state): return run('test_02_07', state, deps=['test_01_08'])
 
 # parent of 2 children in layer3
-@dmark(with_state=True, after=['test_01_08'], tag='layer2')
+@dmark(with_state=True, after=['test_01_08'], tags='layer2')
 def test_02_08(state): return run('test_02_08', state, deps=['test_01_08'])
 
-@dmark(with_state=True, after=['test_01_08'], tag='layer2')
+@dmark(with_state=True, after=['test_01_08'], tags='layer2')
 def test_02_09(state): return run('test_02_09', state, deps=['test_01_08'])
 
 # ---------------------------------------------------------------------------
@@ -114,30 +114,30 @@ def test_02_09(state): return run('test_02_09', state, deps=['test_01_08'])
 #   - test_03_07 has 1 child (layer4)
 # ---------------------------------------------------------------------------
 # parent of 3 children in layer4
-@dmark(with_state=True, after=['test_02_02'], tag='layer3')
+@dmark(with_state=True, after=['test_02_02'], tags='layer3')
 def test_03_01(state): return run('test_03_01', state, deps=['test_02_02'])
 
-@dmark(with_state=True, after=['test_02_02'], tag='layer3')
+@dmark(with_state=True, after=['test_02_02'], tags='layer3')
 def test_03_02(state): return run('test_03_02', state, deps=['test_02_02'])
 
-@dmark(with_state=True, after=['test_02_02'], tag='layer3')
+@dmark(with_state=True, after=['test_02_02'], tags='layer3')
 def test_03_03(state): return run('test_03_03', state, deps=['test_02_02'])
 
 # parent of 1 child in layer4
-@dmark(with_state=True, after=['test_02_06'], tag='layer3')
+@dmark(with_state=True, after=['test_02_06'], tags='layer3')
 def test_03_04(state): return run('test_03_04', state, deps=['test_02_06'])
 
-@dmark(with_state=True, after=['test_02_06'], tag='layer3')
+@dmark(with_state=True, after=['test_02_06'], tags='layer3')
 def test_03_05(state): return run('test_03_05', state, deps=['test_02_06'])
 
-@dmark(with_state=True, after=['test_02_08'], tag='layer3')
+@dmark(with_state=True, after=['test_02_08'], tags='layer3')
 def test_03_06(state): return run('test_03_06', state, deps=['test_02_08'])
 
 # parent of 1 child in layer4
-@dmark(with_state=True, after=['test_02_08'], tag='layer3')
+@dmark(with_state=True, after=['test_02_08'], tags='layer3')
 def test_03_07(state): return run('test_03_07', state, deps=['test_02_08'])
 
-@dmark(with_state=True, after=['test_02_04'], tag='layer3')
+@dmark(with_state=True, after=['test_02_04'], tags='layer3')
 def test_03_08(state): return run('test_03_08', state, deps=['test_02_04'])
 
 # ---------------------------------------------------------------------------
@@ -147,20 +147,20 @@ def test_03_08(state): return run('test_03_08', state, deps=['test_02_04'])
 #   - child of test_03_07: 04_05
 #   - test_04_02 has 2 children (layer5)
 # ---------------------------------------------------------------------------
-@dmark(with_state=True, after=['test_03_01'], tag='layer4')
+@dmark(with_state=True, after=['test_03_01'], tags='layer4,capA')
 def test_04_01(state): return run('test_04_01', state, deps=['test_03_01'])
 
 # parent of 2 children in layer5
-@dmark(with_state=True, after=['test_03_01'], tag='layer4')
+@dmark(with_state=True, after=['test_03_01'], tags='layer4,capA')
 def test_04_02(state): return run('test_04_02', state, deps=['test_03_01'])
 
-@dmark(with_state=True, after=['test_03_01'], tag='layer4')
+@dmark(with_state=True, after=['test_03_01'], tags='layer4,capA')
 def test_04_03(state): return run('test_04_03', state, deps=['test_03_01'])
 
-@dmark(with_state=True, after=['test_03_04'], tag='layer4')
+@dmark(with_state=True, after=['test_03_04'], tags='layer4,capB')
 def test_04_04(state): return run('test_04_04', state, deps=['test_03_04'])
 
-@dmark(with_state=True, after=['test_03_07'], tag='layer4')
+@dmark(with_state=True, after=['test_03_07'], tags='layer4,capB')
 def test_04_05(state): return run('test_04_05', state, deps=['test_03_07'])
 
 # ---------------------------------------------------------------------------
@@ -169,19 +169,19 @@ def test_04_05(state): return run('test_04_05', state, deps=['test_03_07'])
 #   - 05_03 is child of 04_04
 #   - test_05_02 has 1 child (layer6)
 # ---------------------------------------------------------------------------
-@dmark(with_state=True, after=['test_04_02'], tag='layer5')
+@dmark(with_state=True, after=['test_04_02'], tags='layer5')
 def test_05_01(state): return run('test_05_01', state, deps=['test_04_02'])
 
 # parent of 1 child in layer6
-@dmark(with_state=True, after=['test_04_02'], tag='layer5')
+@dmark(with_state=True, after=['test_04_02'], tags='layer5')
 def test_05_02(state): return run('test_05_02', state, deps=['test_04_02'])
 
-@dmark(with_state=True, after=['test_04_04'], tag='layer5')
+@dmark(with_state=True, after=['test_04_04'], tags='layer5')
 def test_05_03(state): return run('test_05_03', state, deps=['test_04_04'])
 
 # ---------------------------------------------------------------------------
 # Layer 6 (1 test)
 #   - child of test_05_02: 06_01
 # ---------------------------------------------------------------------------
-@dmark(with_state=True, after=['test_05_02'], tag='layer6')
+@dmark(with_state=True, after=['test_05_02'], tags='layer6')
 def test_06_01(state): return run('test_06_01', state, deps=['test_05_02'])
