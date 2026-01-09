d787c038bff81a4c66f8612265e7acc057a7bdc6
Emilio Reyes <soda480@gmail.com>
2026-01-09 03:12:11 +0000

    feat: format main thread log formatter
    Signed-off-by: Emilio Reyes <soda480@gmail.com>

--- threaded_order/logger.py
+++ threaded_order/logger.py
@@ -27,9 +27,10 @@ if HAS_COLOR:
             (re.compile(r'Scheduler::State:\s*(\{.*?^})', re.DOTALL | re.MULTILINE), Fore.MAGENTA)
         ]
 
-        def __init__(self, *args, **kwargs):
+        def __init__(self, workers, *args, **kwargs):
             self.highlights = kwargs.pop('highlights', []) or self.DEFAULT_HIGHLIGHTS
             self.verbose = kwargs.pop('verbose', False)
+            self.workers = workers
             super().__init__(*args, **kwargs)
 
         def _apply_highlights(self, message):
@@ -50,8 +51,10 @@ if HAS_COLOR:
 
             # only log thread name if it's not MainThread
             # to avoid cluttering the logs with 'MainThread' entries
-            _thread_name = record.threadName if record.threadName != 'MainThread' else ''
-            thread_name = f'[{_thread_name}] ' if _thread_name else ''
+            if record.threadName == 'MainThread':
+                thread_name = ''
+            else:
+                thread_name = f'[{self.pad_thread_name(record.threadName)}] '
 
             if self.verbose:
                 msg = (
@@ -71,13 +74,30 @@ if HAS_COLOR:
 
             return msg
 
+        def pad_thread_name(self, name):
+            width = len(str(self.workers - 1))
+            return re.sub(r'(\d+)$', lambda m: m.group(1).zfill(width), name)
+
 class MainThreadAwareFormatter(logging.Formatter):
-    def __init__(self, main_fmt, thread_fmt, *args, **kwargs):
+    def __init__(self, main_fmt, thread_fmt, workers, thread_prefix='thread_', *args, **kwargs):
         super().__init__(fmt=main_fmt, *args, **kwargs)
         self.main_fmt = main_fmt
         self.thread_fmt = thread_fmt
+        self.thread_prefix = thread_prefix
+        self.thread_width = len(str(workers - 1))
 
     def format(self, record):
+        if record.threadName.startswith(self.thread_prefix):
+            try:
+                idx = int(record.threadName[len(self.thread_prefix):])
+                padded = f'{self.thread_prefix}{idx:0{self.thread_width}d}'
+            except ValueError:
+                padded = record.threadName
+        else:
+            padded = record.threadName
+        
+        record.paddedThreadName = padded
+
         if record.threadName == 'MainThread':
             self._style._fmt = self.main_fmt
         else:
@@ -104,11 +124,11 @@ def configure_logging(workers, prefix='thread', add_stream_handler=False, highli
     if add_stream_handler or verbose:
         if HAS_COLOR:
             init(autoreset=False)
-            stream_formatter = ColoredFormatter(highlights=highlights, verbose=verbose)
+            stream_formatter = ColoredFormatter(workers, highlights=highlights, verbose=verbose)
         else:
-            main_fmt = '$%(asctime)s %(message)s'
-            thread_fmt = '%(asctime)s [%(threadName)s] %(message)s'
-            stream_formatter = MainThreadAwareFormatter(main_fmt, thread_fmt)
+            main_fmt = '%(asctime)s %(message)s'
+            thread_fmt = '%(asctime)s [%(paddedThreadName)s] %(message)s'
+            stream_formatter = MainThreadAwareFormatter(main_fmt, thread_fmt, workers)
 
         stream_handler = logging.StreamHandler(sys.stderr)
         stream_handler.setFormatter(stream_formatter)
